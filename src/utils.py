5218f5eba8191111ef7f8430ec0c08f566b19477
Joseph Micallef <micallefje@gmail.com>
2025-02-11 16:06:00 +0000

    add removal of italics with test

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,18 @@
 import re
 
 def extract_text_from_srt(filepath):
-    pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*(.*)'
+    timestamp_pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*(.*)'
+    italic_pattern = r'<(/?i)>'
 
     with open(filepath) as f:
         text = f.read()
-        text_lines = re.findall(pattern, text)
     
+    text_lines = re.findall(timestamp_pattern, text)
+    cleaned_text = '\n\n'.join(text_lines)
+    cleaned_text = re.sub(italic_pattern, "", cleaned_text)
+
     with open(filepath, "w") as new_file:
-        new_file.write('\n\n'.join(text_lines))
+        new_file.write(cleaned_text)
 
 extract_text_from_srt('example.srt')
 
--- test/test_utils.py
+++ test/test_utils.py
@@ -5,17 +5,25 @@ import pytest
 @pytest.fixture
 def example_srt(tmp_path):
     example_srt = tmp_path / 'example.srt'
-    example_srt.write_text('''1
+    return example_srt
+
+class TestExtractTextFromSrt:
+    def test_removes_timestamps(self, example_srt):
+        example_srt.write_text('''1
                         00:00:35,077 --> 00:00:36,203
                         Hello!
 
                         2
                         00:00:41,291 --> 00:00:42,751
                         Hello!''')
-    return example_srt
-
-class TestExtractTextFromSrt:
-    def test_removes_timestamps(self, example_srt):
         expected = '''Hello!\n\nHello!'''
+        extract_text_from_srt(example_srt)
+        assert example_srt.read_text() == expected
+    
+    def test_removes_italic_markers(self, example_srt):
+        example_srt.write_text('''5
+                               00:04:30,604 --> 00:04:32,231
+                               <i>Good morning, Refiners.</i>''')
+        expected = '''Good morning, Refiners.'''
         extract_text_from_srt(example_srt)
         assert example_srt.read_text() == expected
