94bde5a4b38f2fde9e76c0371302430ccb96fd7c
Joseph Micallef <micallefje@gmail.com>
2025-02-11 15:39:00 +0000

    add use of tmp_path

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import re
 
-def extract_text_from_srt(text):
+def extract_text_from_srt(filepath):
     pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*(.*)'
 
-    text_lines = re.findall(pattern, text)
+    with open(filepath) as f:
+        text = f.read()
+        text_lines = re.findall(pattern, text)
 
     return '\n'.join(text_lines)
 
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,13 +1,20 @@
 from src.utils import extract_text_from_srt
+import pytest
 
-class TestExtractTextFromSrt:
-    def test_removes_timestamps(self):
-        test_string = '''1
+
+@pytest.fixture
+def example_srt(tmp_path):
+    example_srt = tmp_path / 'example.srt'
+    example_srt.write_text('''1
                         00:00:35,077 --> 00:00:36,203
                         Hello!
 
                         2
                         00:00:41,291 --> 00:00:42,751
-                        Hello!'''
+                        Hello!''')
+    return example_srt
+
+class TestExtractTextFromSrt:
+    def test_removes_timestamps(self, example_srt):
         expected = '''Hello!\nHello!'''
-        assert extract_text_from_srt(test_string) == expected
+        assert extract_text_from_srt(example_srt) == expected
