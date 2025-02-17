c2a56cc5866705334d4e4c1b23e9f5320ccc2f11
Joseph Micallef <micallefje@gmail.com>
2025-02-17 10:01:19 +0000

    replace findall with sub to remove timestamps

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,17 @@
 import re
 
 def extract_text_from_srt(filepath):
-    timestamp_pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*(.*)'
+    timestamp_pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*'
     italic_pattern = r'<(/?i)>'
 
     with open(filepath) as f:
         text = f.read()
     
-    text_lines = re.findall(timestamp_pattern, text)
-    cleaned_text = '\n\n'.join(text_lines)
+    cleaned_text = re.sub(timestamp_pattern, "", text)
     cleaned_text = re.sub(italic_pattern, "", cleaned_text)
 
-    with open(filepath, "w") as new_file:
-        new_file.write(cleaned_text)
+    with open(filepath, "w", encoding="utf-8") as f:
+        f.write(cleaned_text)
 
-extract_text_from_srt('example.srt')
+# extract_text_from_srt('example.srt')
 
