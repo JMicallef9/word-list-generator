f4455b74cefcf2a834f7b55b7acc6b5bf48600bd
Joseph Micallef <micallefje@gmail.com>
2025-02-17 09:59:59 +0000

    fix indentation in tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -9,13 +9,11 @@ def example_srt(tmp_path):
 
 class TestExtractTextFromSrt:
     def test_removes_timestamps(self, example_srt):
-        example_srt.write_text('''1
-                        00:00:35,077 --> 00:00:36,203
-                        Hello!
-
-                        2
-                        00:00:41,291 --> 00:00:42,751
-                        Hello!''')
+        example_srt.write_text("""1
+                               00:00:35,077 --> 00:00:36,203
+                               Hello!\n\n2
+                               00:00:41,291 --> 00:00:42,751
+                               Hello!""")
         expected = '''Hello!\n\nHello!'''
         extract_text_from_srt(example_srt)
         assert example_srt.read_text() == expected
@@ -29,12 +27,9 @@ class TestExtractTextFromSrt:
         assert example_srt.read_text() == expected
     
     def test_handles_multiple_lines_of_text(self, example_srt):
-        example_srt.write_text('''
-                                6
+        example_srt.write_text('''6
                                 00:04:33,232 --> 00:04:36,359
-                                <i>This is Mr. Milchick from work,
-                                and I'm thrilled to welcome you</i>''')
-        expected = '''This is Mr. Milchick from work,
-                    and I'm thrilled to welcome you'''
+                                <i>This is Mr. Milchick from work,\nand I'm thrilled to welcome you</i>''')
+        expected = '''This is Mr. Milchick from work,\nand I'm thrilled to welcome you'''
         extract_text_from_srt(example_srt)
         assert example_srt.read_text() == expected
