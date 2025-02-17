63ec4998b25db7c5df57db524f417d780ac35715
Joseph Micallef <micallefje@gmail.com>
2025-02-17 16:14:45 +0000

    simplify with general pattern matching

--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,13 @@ import re
 
 def extract_text_from_srt(filepath):
     timestamp_pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*'
-    italic_pattern = r'<(/?i)>'
+    tag_pattern = r'<.*?>'
+    combined_pattern = rf"{timestamp_pattern}|{tag_pattern}"
 
     with open(filepath) as f:
         text = f.read()
     
-    cleaned_text = re.sub(timestamp_pattern, "", text)
-    cleaned_text = re.sub(italic_pattern, "", cleaned_text)
+    cleaned_text = re.sub(combined_pattern, "", text)
 
     with open(filepath, "w", encoding="utf-8") as f:
         f.write(cleaned_text)
