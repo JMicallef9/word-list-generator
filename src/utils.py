2316a3008dc7c60c297ceb50a903c0fb849d91c8
Joseph Micallef <micallefje@gmail.com>
2025-02-20 12:21:52 +0000

    add error handling with pathlib

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,30 @@
 import re
+from pathlib import Path
+
 
 def extract_text_from_srt(filepath):
-    timestamp_pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*'
-    tag_pattern = r'<.*?>'
-    combined_pattern = rf"{timestamp_pattern}|{tag_pattern}"
+    valid_formats = ['.srt', '.txt', '.md']
 
-    with open(filepath) as f:
-        text = f.read()
+    filepath = Path(filepath)
+
+    if not filepath.exists():
+        raise FileNotFoundError(f"Error: The file '{filepath}' was not found.")
     
-    cleaned_text = re.sub(combined_pattern, "", text)
+    if not any(filepath.suffix == ext for ext in valid_formats):
+        raise IOError(f"Error: Could not read the file contents of '{filepath.name}'. File format is invalid.")
+    
+    try:
+        timestamp_pattern = r'\d+\s+\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\s*'
+        tag_pattern = r'<.*?>'
+        combined_pattern = rf"{timestamp_pattern}|{tag_pattern}"
 
-    with open(filepath, "w", encoding="utf-8") as f:
-        f.write(cleaned_text)
+        with open(filepath) as f:
+            text = f.read()
+            
+        cleaned_text = re.sub(combined_pattern, "", text)
 
-# extract_text_from_srt('example.srt')
-
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.write(cleaned_text)
+        
+    except RuntimeError:
+        raise RuntimeError(f"Error: Could not read the file '{filepath}'")
