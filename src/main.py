0bbadeefdaa8bdeed3ef0675d8152e1b579e6a07
Joseph Micallef <micallefje@gmail.com>
2025-02-23 19:02:08 +0000

    create main.py

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from utils import extract_text_from_srt, generate_word_list, check_for_new_words, convert_word_list_to_csv
+from anki_utils import get_anki_decks, get_words_from_deck
+
+def word_list_generator():
+    pass
