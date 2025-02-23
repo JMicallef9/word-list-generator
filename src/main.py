480885af661bb75973fcc0f849cb4910f75d3ebd
Joseph Micallef <micallefje@gmail.com>
2025-02-23 21:05:01 +0000

    begin writing main script

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-from utils import extract_text_from_srt, generate_word_list, check_for_new_words, convert_word_list_to_csv
+from utils import extract_text_from_file, generate_word_list, check_for_new_words, convert_word_list_to_csv
 from anki_utils import get_anki_decks, get_words_from_deck
 
 def word_list_generator():
-    pass
+    file_to_process = input("Please enter the filepath to be processed.")
+
+    text = extract_text_from_file(file_to_process)
+
+    
+
