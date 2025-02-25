d722c6932fa334e3cfdd3e8afcef493cc84ae8b5
Joseph Micallef <micallefje@gmail.com>
2025-02-25 12:45:14 +0000

    correct user inputs in main script

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from utils import extract_text_from_file, generate_word_list, check_for_new_words, convert_word_list_to_csv, get_user_language
 from anki_utils import get_anki_decks, get_words_from_deck
-from deep_translator import GoogleTranslator
 
 
 def word_list_generator():
     file_to_process = input("Please enter the filepath to be processed: ")
+    # FileNotFoundError
 
     text = extract_text_from_file(file_to_process)
 
@@ -16,13 +16,17 @@ def word_list_generator():
     else:
         csv_name = input("Please enter a filename/directory for the CSV file: ")
 
-        text_lang = get_user_language('What language is your text in?')
+        print('What language is your text in?')
+        text_lang = get_user_language()
 
-        translation_lang = get_user_language('What language would you like translations in?')
+        print('What language would you like translations in?')
+        translation_lang = get_user_language()
 
-        convert_word_list_to_csv(text, csv_name, text_lang, translation_lang)
+        word_counts = generate_word_list(text)
+
+        convert_word_list_to_csv(word_counts, csv_name, text_lang, translation_lang)
 
         print(f"Word list file created: {csv_name}")
 
-
-
+if __name__ == '__main__':
+    word_list_generator()
