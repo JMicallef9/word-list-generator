33fce5aa4252c36a86fe013464f678a317272a86
Joseph Micallef <micallefje@gmail.com>
2025-02-25 17:12:37 +0000

    add checks for valid output filepath

--- src/main.py
+++ src/main.py
@@ -1,25 +1,37 @@
 from utils import extract_text_from_file, generate_word_list, check_for_new_words, convert_word_list_to_csv, get_user_language
 from anki_utils import get_anki_decks, get_words_from_deck
+from pathlib import Path
 
 
 def word_list_generator():
-    file_to_process = input("Please enter the filepath to be processed: ")
-    # FileNotFoundError
+    while True:
+        file_to_process = input("Enter the filepath of the file you want to process: ")
+        if Path(file_to_process).is_file():
+            break
+        else:
+            print("Invalid filepath. Please provide a valid filepath (e.g., /path/to/input.txt).")
 
     text = extract_text_from_file(file_to_process)
 
-    anki_check = input("Do you want to filter the list using an Anki deck? (Y/n): ").strip().lower()
+    anki_check = input("\nDo you want to filter the list using an Anki deck? (Y/n): ").strip().lower()
 
     if anki_check == 'y':
         pass
 
     else:
-        csv_name = input("Please enter a filename/directory for the CSV file: ")
 
-        print('What language is your text in?')
+        while True:
+            csv_name = input("\nPlease enter the destination filepath for the output CSV file: ")
+            if Path(csv_name).parent and not Path(csv_name).exists():
+                print("Invalid filepath. Please provide a valid filepath (e.g., /path/to/input.txt).")
+                continue
+            break
+
+
+        print('\nWhat language is your text in?')
         text_lang = get_user_language()
 
-        print('What language would you like translations in?')
+        print('\nWhat language would you like translations in?')
         translation_lang = get_user_language()
 
         word_counts = generate_word_list(text)
