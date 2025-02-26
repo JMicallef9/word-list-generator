3c6eb6565d337e41b2251f980b531f11b49d3f69
Joseph Micallef <micallefje@gmail.com>
2025-02-26 13:24:03 +0000

    add use of MagicMock for correct JSON testing

--- test/test_anki_utils.py
+++ test/test_anki_utils.py
@@ -1,14 +1,17 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from src.anki_utils import get_anki_decks, get_words_from_deck
 
 @pytest.fixture
 def mock_anki_post():
     with patch("requests.post") as mock_post:
-        mock_post.side_effect = [{
+        first_mock_response = MagicMock()
+        first_mock_response.json.return_value = {
     "result": [1483959289817, 1483959291695],
     "error": None
-}, {
+}
+        second_mock_response = MagicMock()
+        second_mock_response.json.return_value = {
     "result": [
         {
             "noteId":1502298033753,
@@ -20,8 +23,8 @@ def mock_anki_post():
             }
         }
     ],
-    "error": None
-}]
+    "error": None}
+        mock_post.side_effect = [first_mock_response, second_mock_response]
         yield mock_post
 
 @patch("requests.post")
@@ -62,10 +65,13 @@ class TestGetWordsFromDeck:
     
     def test_capitalisation_ignored_in_output(self):
         with patch("requests.post") as mock_post:
-            mock_post.side_effect = [{
+            first_response = MagicMock()
+            first_response.json.return_value = {
     "result": [1483959289817, 1483959291695],
     "error": None
-}, {
+} 
+            second_response = MagicMock()
+            second_response.json.return_value = {
     "result": [
         {
             "noteId":1502298033753,
@@ -78,16 +84,20 @@ class TestGetWordsFromDeck:
         }
     ],
     "error": None
-}]
+}
+            mock_post.side_effect = [first_response, second_response]
             output = get_words_from_deck('deck_name')
             assert output == {'hello'}
     
     def test_punctuation_ignored_in_output(self):
         with patch("requests.post") as mock_post:
-            mock_post.side_effect = [{
+            first_response = MagicMock()
+            first_response.json.return_value = {
     "result": [1483959289817, 1483959291695],
     "error": None
-}, {
+} 
+            second_response = MagicMock()
+            second_response.json.return_value = {
     "result": [
         {
             "noteId":1502298033753,
@@ -100,17 +110,21 @@ class TestGetWordsFromDeck:
         }
     ],
     "error": None
-}]
+}
+            mock_post.side_effect = [first_response, second_response]
             output = get_words_from_deck('deck_name')
             assert output == {'hello', 'okay', 'everybody', 'meaning'}
         
     
     def test_Spanish_punctuation_ignored_in_output(self):
         with patch("requests.post") as mock_post:
-            mock_post.side_effect = [{
+            first_response = MagicMock()
+            first_response.json.return_value = {
     "result": [1483959289817, 1483959291695],
     "error": None
-}, {
+} 
+            second_response = MagicMock()
+            second_response.json.return_value = {
     "result": [
         {
             "noteId":1502298033753,
@@ -123,17 +137,21 @@ class TestGetWordsFromDeck:
         }
     ],
     "error": None
-}]
+}
+            mock_post.side_effect = [first_response, second_response]
             output = get_words_from_deck('deck_name')
             assert output == {'te', 'lo', 'ruego', 'y', 'tú'}
         
     
     def test_maintains_hyphenation_in_compound_words(self):
         with patch("requests.post") as mock_post:
-            mock_post.side_effect = [{
+            first_response = MagicMock()
+            first_response.json.return_value = {
     "result": [1483959289817, 1483959291695],
     "error": None
-}, {
+} 
+            second_response = MagicMock()
+            second_response.json.return_value = {
     "result": [
         {
             "noteId":1502298033753,
@@ -146,16 +164,20 @@ class TestGetWordsFromDeck:
         }
     ],
     "error": None
-}]
+}
+            mock_post.side_effect = [first_response, second_response]
             output = get_words_from_deck('deck_name')
             assert output == {'físico-químico', 'test'}
         
     def test_compiles_set_from_multiple_cards(self):
         with patch("requests.post") as mock_post:
-            mock_post.side_effect = [{
+            first_response = MagicMock()
+            first_response.json.return_value = {
     "result": [1483959289817, 1483959291695],
     "error": None
-}, {
+} 
+            second_response = MagicMock()
+            second_response.json.return_value = {
     "result": [
         {
             "noteId":1502298033753,
@@ -176,17 +198,21 @@ class TestGetWordsFromDeck:
         }
     ],
     "error": None
-}]
+}
+            mock_post.side_effect = [first_response, second_response]
             output = get_words_from_deck('deck_name')
             assert output == {'físico-químico', 'test', 'front', 'content', 'back'}
 
 
     def test_multiple_punctuation_characters_ignored_in_output(self):
         with patch("requests.post") as mock_post:
-            mock_post.side_effect = [{
+            first_response = MagicMock()
+            first_response.json.return_value = {
     "result": [1483959289817, 1483959291695],
     "error": None
-}, {
+} 
+            second_response = MagicMock()
+            second_response.json.return_value = {
     "result": [
         {
             "noteId":1502298033753,
@@ -199,6 +225,34 @@ class TestGetWordsFromDeck:
         }
     ],
     "error": None
-}]
+}
+            mock_post.side_effect = [first_response, second_response]
             output = get_words_from_deck('deck_name')
             assert output == {'hello', 'okay', 'everybody', 'meaning'}
+    
+
+    def test_strips_out_html_tags(self):
+        with patch("requests.post") as mock_post:
+            first_response = MagicMock()
+            first_response.json.return_value = {
+    "result": [1483959289817, 1483959291695],
+    "error": None
+} 
+            second_response = MagicMock()
+            second_response.json.return_value = {
+    "result": [
+        {
+            "noteId":1502298033753,
+            "modelName": "Basic",
+            "tags":["tag","another_tag"],
+            "fields": {
+                "Front": {"value": "hello<br>goodbye.", "order": 0},
+                "Back": {"value": "yes<br><br>no", "order": 1}
+            }
+        }
+    ],
+    "error": None
+}
+            mock_post.side_effect = [first_response, second_response]
+            output = get_words_from_deck('deck_name')
+            assert output == {'hello', 'goodbye', 'yes', 'no'}
